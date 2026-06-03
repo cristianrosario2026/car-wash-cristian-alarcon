ac205fd3682444a161eb6ae3c0b52d47f3602ddf
cristianrosario2026 <cristianrosarioalarconalarcon@gmail.com>
2026-06-02 20:34:20 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 AOS.init();
 
-function cerrarMenu() {
-    document.getElementById("menu").checked = false;
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
 }
+
+const links = document.querySelectorAll("nav ul li a");
+const nav = document.getElementById("nav");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+
+        // quitar la clase a todos
+        links.forEach(l => l.classList.remove("seleccionado"));
+
+        // agregar al que clickeaste
+        this.classList.add("seleccionado");
+
+        // 🔥 cerrar menú en móvil
+        nav.className = "";
+    });
+});
